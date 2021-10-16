bc996f713f36771b8cfb4c466dbf7e4fe5fe8ba0
Alim ARSLAN <alimarslan003@gmail.com>
2021-10-16 16:53:20 +0300

    UnityLearnLessons1and2

---
+++ Unity 2. Tutorial/Prototype 2/Assets/Course Library/Scripts/DestroyOutOfBounds.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOutOfBounds : MonoBehaviour
+{
+    private float topBound = 30;
+    private float lowerBound = -10;
+    private float sideBound = 30;
+    // Start is called before the first frame update
+    private GameManager gameManager;
+    void Start()
+    {
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //If an object goes past the player view in the game, remove that object 
+        if (transform.position.z > topBound)
+        {
+            Destroy(gameObject);
+        }
+        else if (transform.position.z < lowerBound)
+        {
+            gameManager.AddLives(-1);
+            Destroy(gameObject);
+        }
+        else if (transform.position.x > sideBound)
+        {
+            gameManager.AddLives(-1);
+            Destroy(gameObject);
+        }
+        else if (transform.position.x < -sideBound)
+        {
+            gameManager.AddLives(-1);
+            Destroy(gameObject);
+        }
+    }
+}
